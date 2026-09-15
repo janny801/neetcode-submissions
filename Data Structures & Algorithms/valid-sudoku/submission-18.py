1ecef78ac716a245ecef8d66bf0a6c0cfc5a634f
Janred Salubayba <jred8069@gmail.com>
2026-09-15 10:56:36 -0500

    Add: valid-sudoku - submission-18

---
+++ Data Structures & Algorithms/valid-sudoku/submission-18.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes= [set() for _ in range(9)]
+        for row in range(9): 
+            for col in range(9): 
+                currval = board[row][col]
+                if currval ==".": 
+                    continue 
+                boxidx = (row//3)*3 + (col//3)
+                if currval in rows[row] or currval in cols[col] or currval in box[boxidx]: 
+                    return False
+                rows[row].add(currval)
+                cols[col].add(currval)
+                boxes[boxidx].add(currval)
+        return True
+        
